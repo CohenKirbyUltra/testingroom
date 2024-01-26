8f9024554494fb44e1bc5a211d1a9a4fd5594748
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-01-26 22:14:37 +0000

    fish

--- src/find.js
+++ src/find.js
@@ -1,16 +1,17 @@
 var times;
 var image = document.getElementById("findmeplease");
-const tests = ["../imgs/Test1.png", "../imgs/Test2.png", "../imgs/Test3.png", "../imgs/Test4.png", "../imgs/Test5.png", "../imgs/Test6.png"];
+const tests = ["../../imgs/Test1.png", "../imgs/../Test2.png", "../../imgs/Test3.png", "../../imgs/Test4.png", "../imgs/../Test5.png", "../imgs/../Test6.png"];
 
 function update() {
     if (times < 5) {
         image.src = tests[times];
-        times++;
     } else {
         if (times === 5) {
             PlayAudio();
+            window.alert("why");
         }
     }
+    times++;
 }
 document.onvisibilitychange = () => {
     if (document.visibilityState === "hidden") {
