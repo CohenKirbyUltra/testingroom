3dd9809746ff1aa7208c08f0785ff505f4db3994
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-01-26 21:55:43 +0000

    findme

--- src/find.js
+++ src/find.js
@@ -1,6 +1,6 @@
 var times;
 
-document.addEventListener("visibilitychange", function () {
+window.document.addEventListener("visibilitychange", function () {
   if (this.visibilityState === "hidden") {
     update();
   }
@@ -10,11 +10,11 @@ var image = document.getElementById("findmeplease");
 const tests = ["imgs/Test1.png", "imgs/Test2.png", "imgs/Test3.png", "imgs/Test4.png", "imgs/Test5.png", "imgs/Test6.png"];
 
 function update() {
-    if (times < tests.length) {
+    if (times < 5) {
         image.src = tests[times];
         times++;
     } else {
-        if (times === tests.length) {
+        if (times === 5) {
             PlayAudio();
         }
     }
