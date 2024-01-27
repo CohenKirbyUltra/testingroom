810618ed85741d996989811e0eeb4d31f3e61659
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-01-27 15:34:59 +0000

    bug fixs

--- src/dakota.js
+++ src/dakota.js
@@ -3,6 +3,10 @@ Sfx.src = "../audio/music/torture.mp3";
 Sfx.loop = true;
 Sfx.type = "audio/mpeg";
 
+var vocal = document.getElementById("vocal");
+vocal.loop = true;
+vocal.type = "audio/wav";
+
 var Music = new Audio();
 Music.src = "../audio/music/Theresalwayssomething.mp3";
 Music.loop = true;
@@ -26,21 +30,46 @@ document.onvisibilitychange = () => {
             document.body.appendChild(para);
         }, 100)
       });
+        vocal.pause();
+        vocal.currentTime = 0;
       document.getElementById("torture").play();
     }
     times++;
       switch (times) {
         case 1:
+          vocal.pause();
+          vocal.currentTime = 0;
+          vocal.src = "https://cohenkirbyultra.github.io/testingroom/audio/sfx/dontleavemehere.wav";
+          vocal.play();
           alert("Don't leave me here...");
           break;
         case 2:
+          vocal.pause();
+          vocal.currentTime = 0;
+          vocal.src = "https://cohenkirbyultra.github.io/testingroom/audio/sfx/please.wav";
+          vocal.play();
           alert("Please..");
           break;
         case 3:
+          vocal.pause();
+          vocal.currentTime = 0;
+          vocal.src = "https://cohenkirbyultra.github.io/testingroom/audio/sfx/cohen.wav";
+          vocal.play();
           alert("PLEASE I'M BEGGING YOU C||||E|");
           break;
         case 4:
+          vocal.pause();
+          vocal.currentTime = 0;
+          vocal.src = "https://cohenkirbyultra.github.io/testingroom/audio/sfx/whatdididowrong.wav";
+          vocal.play();
           alert("WHAT DID I DO WRONG?!");
+          break;
+        case 5:
+          vocal.pause();
+          vocal.currentTime = 0;
+          vocal.src = "https://cohenkirbyultra.github.io/testingroom/audio/sfx/ithurts.mp3";
+          vocal.play();
+          alert("#######################");
           break;
         default:
           break;
