854c00d34d64bf0925ac530863bb822f159a26cf
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-01-27 05:18:14 +0000

    ughhh

--- src/karma.js
+++ src/karma.js
@@ -8,92 +8,97 @@ var currentValue = "";
 Happybtn.disabled = true;
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-window.addEventListener("keypress", function() {
-    currentValue = InputerCode.value;
+window.addEventListener("keypress", function () {
+  currentValue = InputerCode.value;
 
-    if (currentValue > 0) {
-        Happybtn.disabled = false;
-        Happybtn.innerHTML = "Happy";
-    } else {
-        Happybtn.enabled = true;
-        Happybtn.innerHTML = "...";
+  if (currentValue > 0) {
+    Happybtn.disabled = false;
+    Happybtn.innerHTML = "Happy";
+  } else {
+    Happybtn.enabled = true;
+    Happybtn.innerHTML = "...";
+  }
+});
+document.getElementById("submithappiness").onClick = function () {
+  if (!currentValue === codes[0] && !currentValue === codes[1]) {
+    alert("Failure.");
+    location.replace(
+      "https://cohenkirbyultra.github.io/testingroom/index.html",
+    );
+  } else {
+    if (currentValue === codes[0]) {
+      location.replace(
+        "https://cohenkirbyultra.github.io/testingroom/pages/smile/Cohen.html",
+      );
     }
-})
-document.getElementById("submithappiness").onClick = function() {
-    if (!currentValue === codes[0] && !currentValue === codes[1]) {
-        alert("Failure.")
-        location.replace("https://cohenkirbyultra.github.io/testingroom/index.html");
-    } else {
-        if (currentValue === codes[0]) {
-            location.replace("https://cohenkirbyultra.github.io/testingroom/pages/smile/Cohen.html"); 
-        }
-        if (currentValue === codes[1]) {
-            alert("How do you know that?");
-            location.replace("https://cohenkirbyultra.github.io/testingroom/index.html");
-        }
+    if (currentValue === codes[1]) {
+      alert("How do you know that?");
+      location.replace(
+        "https://cohenkirbyultra.github.io/testingroom/index.html",
+      );
     }
-}
+  }
+};
 
 // sfx stuff
-var Sfx = document.getElementById("keysound");
-Sfx.src = "audio/sfx/key1.wav";
-Sfx.loop = false;
-Sfx.type = "audio/wav";
+document.getElementById("keysound").src = "audio/sfx/key1.wav";
+document.getElementById("keysound").loop = false;
+document.getElementById("keysound").type = "audio/wav";
 //Audio Keys
 function PlaySfx(type) {
-    switch(type) {
+  document.getElementById("keysound").pause();
+  document.getElementById("keysound").currentTime = 0;
+  switch (type) {
     case 0:
-            Sfx.src = "audio/sfx/key1.wav";                
-            break;
+      document.getElementById("keysound").src = "audio/sfx/key1.wav";
+      break;
     case 1:
-            Sfx.src = "audio/sfx/key2.wav";  
-            break;
+      document.getElementById("keysound").src = "audio/sfx/key2.wav";
+      break;
     case 2:
-            Sfx.src = "audio/sfx/key3.wav";  
-            break;
+      document.getElementById("keysound").src = "audio/sfx/key3.wav";
+      break;
     case 3:
-            Sfx.src = "audio/sfx/key4.wav";  
-            break;
+      document.getElementById("keysound").src = "audio/sfx/key4.wav";
+      break;
     case 4:
-            Sfx.src = "audio/sfx/key5.wav";  
-            break;
+      document.getElementById("keysound").src = "audio/sfx/key5.wav";
+      break;
     default:
-            console.log("Error, over sfx limit");
-            alert("Actual dev error: rng over limit of sfx type");
-            break;
-    }
-            // immediately play
-            Sfx.pause();
-            Sfx.currentTime = 0;
-            Sfx.play();
+      console.log("Error, over sfx limit");
+      alert("Actual dev error: rng over limit of sfx type");
+      break;
+  }
+  // immediately play
+  document.getElementById("keysound").play();
 }
 
 // generates random number to play sfx randomly
 function KeyClick() {
-    let Key = getRndInteger(0, 4);
+  let Key = getRndInteger(0, 4);
 
-    switch (Key) {
-        case 0:
-            PlaySfx(0);
-            break;
-        case 1:
-            PlaySfx(1);
-            break;
-        case 2:
-            PlaySfx(2);
-            break;
-        case 3:
-            PlaySfx(3);
-            break;
-        case 4:
-            PlaySfx(4);
-            break;
-        default:
-            console.log("Error, over sfx limit");
-            alert("Actual dev error: rng over limit of sfx type");
-            break;
-    }
+  switch (Key) {
+    case 0:
+      PlaySfx(0);
+      break;
+    case 1:
+      PlaySfx(1);
+      break;
+    case 2:
+      PlaySfx(2);
+      break;
+    case 3:
+      PlaySfx(3);
+      break;
+    case 4:
+      PlaySfx(4);
+      break;
+    default:
+      console.log("Error, over sfx limit");
+      alert("Actual dev error: rng over limit of sfx type");
+      break;
+  }
 }
