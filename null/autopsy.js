8860b771474779474d6223cb26f2726efba6c942
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-01-28 15:06:26 +0000

    d

--- null/autopsy.js
+++ null/autopsy.js
@@ -2,6 +2,7 @@ var LOSTAUDIO = new Audio();
 LOSTAUDIO.src = "lostlostlostlostlost.mp3";
 LOSTAUDIO.loop = true;
 LOSTAUDIO.type = "audio/mp3";
+LOSTAUDIO.preload;
 
 document.onload = () => {
   LOSTAUDIO.play();
