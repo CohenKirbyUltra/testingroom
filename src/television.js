bd72606d7d80e09d28fde695fc957fb3bdd9f489
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-02-06 21:59:26 +0000

    tv test

---
+++ src/television.js
@@ -0,0 +1,126 @@
+var Noise = new Audio();
+Noise.src = "audio/sfx/Static.mp3";
+Noise.loop = true;
+Noise.volume = 0.5;
+
+Noise.play();
+
+const codes = ["15", "1777"];
+
+var InputerCode = document.getElementById("vhsInput");
+var Happybtn = document.getElementById("vhsSubmit");
+var TV = document.getElementById("vhsplayer");
+var currentValue = "";
+Happybtn.disabled = true;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+setInterval(function () {
+  currentValue = InputerCode.value;
+
+  if (currentValue > 0) {
+    Happybtn.disabled = false;
+    Happybtn.innerHTML = "Play";
+  } else {
+    Happybtn.enabled = true;
+    Happybtn.innerHTML = "...";
+  }
+}, 500);
+
+window.addEventListener("keypress", function () {
+  KeyClick();
+});
+
+document.getElementById("vhsSubmit").addEventListener("click", Submit);
+
+function Submit() {
+  switch (currentValue) {
+    case codes[0]:
+      vhsplayer.src = "https://www.youtube.com/watch?v=RGS_ConjNXU";
+      vhsplayer.hidden = false;
+      vhsplayer.play();
+      break;
+    case codes[1]:
+      vhsplayer.src = "https://www.youtube.com/watch?v=5E4TxH6jCwo";
+      vhsplayer.hidden = false;
+      vhsplayer.play();
+      break;
+    default:
+      Failure.play();
+      window.alert("Failure.");
+      setTimeout(function () {
+        location.replace("about:blank");
+      }, 1000);
+      break;
+  }
+}
+
+// sfx stuff
+
+var Keyboard = new Audio();
+Keyboard.src =
+  "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key1.wav";
+Keyboard.loop = false;
+Keyboard.type = "audio/wav";
+//Audio Keys
+function PlaySfx(type) {
+  switch (type) {
+    case 0:
+      Keyboard.src =
+        "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key1.wav";
+      break;
+    case 1:
+      Keyboard.src =
+        "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key2.wav";
+      break;
+    case 2:
+      Keyboard.src =
+        "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key3.wav";
+      break;
+    case 3:
+      Keyboard.src =
+        "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key4.wav";
+      break;
+    case 4:
+      Keyboard.src =
+        "https://cohenkirbyultra.github.io/testingroom/audio/sfx/Key5.wav";
+      break;
+    default:
+      console.log("Error, over sfx limit");
+      alert("Actual dev error: rng over limit of sfx type");
+      break;
+  }
+  // immediately play
+  Keyboard.play();
+}
+
+// generates random number to play sfx randomly
+function KeyClick() {
+  Keyboard.pause();
+  Keyboard.currentTime = 0;
+  let Key = getRndInteger(0, 4);
+
+  switch (Key) {
+    case 0:
+      PlaySfx(0);
+      break;
+    case 1:
+      PlaySfx(1);
+      break;
+    case 2:
+      PlaySfx(2);
+      break;
+    case 3:
+      PlaySfx(3);
+      break;
+    case 4:
+      PlaySfx(4);
+      break;
+    default:
+      console.log("Error, over sfx limit");
+      alert("Actual dev error: rng over limit of sfx type");
+      break;
+  }
+}
