90f1cc84793931c35034669ca451e3f0d2f96365
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2024-08-15 01:10:59 +0000

    q

--- src/globalcursor.js
+++ src/globalcursor.js
@@ -1,29 +1,34 @@
 var clicksfxs, hoversfxs, releasesfxs;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 clicksfxs = [
-  "../audio/ui/sfx_rrui_watch_ui_select_click_01.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_click_02.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_click_03.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_click_04.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_click_05.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_click_06.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_01.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_02.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_03.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_04.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_05.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_click_06.wav",
 ];
 
 hoversfxs = [
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_01.wav",
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_02.wav",
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_03.wav",
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_04.wav",
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_05.wav",
-  "../audio/ui/sfx_rrui_watch_ui_button_hover_general_06.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_01.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_02.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_03.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_04.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_05.wav",
+  "audio/ui/sfx_rrui_watch_ui_button_hover_general_06.wav",
 ];
 
 releasesfxs = [
-  "../audio/ui/sfx_rrui_watch_ui_select_up_01.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_up_02.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_up_03.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_up_04.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_up_05.wav",
-  "../audio/ui/sfx_rrui_watch_ui_select_up_06.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_01.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_02.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_03.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_04.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_05.wav",
+  "audio/ui/sfx_rrui_watch_ui_select_up_06.wav",
 ];
 var click, hover, release;
 click = new Audio();
@@ -35,31 +40,19 @@ hover.type = "audio/wav";
 release = new Audio();
 release.type = "audio/wav";
 
-document.addEventListener("click", function () {
-  sfxplay("click");
-});
-
-document.button.addEventListener("mouseover", function () {
-  sfxplay("hover");
-});
-
 function sfxplay(type) {
   let x = getRndInteger(0, 6);
   switch (type) {
     case "click":
       click.src = clicksfxs[x];
-      click.play();
+      click.cloneNode().play();
     case "hover":
       hover.src = hoversfxs[x];
-      hover.play();
+      hover.cloneNode().play();
     case "release":
       release.src = releasesfxs[x];
-      release.play();
+      release.cloneNode().play();
     default:
     // nothing happens
   }
-}
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
 }
