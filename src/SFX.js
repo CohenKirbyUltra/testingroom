7a60a35bddb80c910b12fda34fe639a1156bf6b6
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-04-20 15:15:55 -0300

    f

--- src/SFX.js
+++ src/SFX.js
@@ -5,33 +5,34 @@ function getRndInteger(min, max) {
 }
 
 clicksfxs = [
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_01.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_02.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_03.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_04.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_05.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_click_06.wav",
+    "sfx_rrui_watch_ui_select_click_01.wav",
+    "sfx_rrui_watch_ui_select_click_02.wav",
+    "sfx_rrui_watch_ui_select_click_03.wav",
+    "sfx_rrui_watch_ui_select_click_04.wav",
+    "sfx_rrui_watch_ui_select_click_05.wav",
+    "sfx_rrui_watch_ui_select_click_06.wav",
 ];
 
 hoversfxs = [
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_01.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_02.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_03.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_04.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_05.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_button_hover_general_06.wav",
+    "sfx_rrui_watch_ui_button_hover_general_01.wav",
+    "sfx_rrui_watch_ui_button_hover_general_02.wav",
+    "sfx_rrui_watch_ui_button_hover_general_03.wav",
+    "sfx_rrui_watch_ui_button_hover_general_04.wav",
+    "sfx_rrui_watch_ui_button_hover_general_05.wav",
+    "sfx_rrui_watch_ui_button_hover_general_06.wav",
 ];
 
 releasesfxs = [
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_01.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_02.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_03.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_04.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_05.wav",
-    "https://cohenkirbyultra.github.io/testingroom/audio/ui/sfx_rrui_watch_ui_select_up_06.wav",
+    "sfx_rrui_watch_ui_select_up_01.wav",
+    "sfx_rrui_watch_ui_select_up_02.wav",
+    "sfx_rrui_watch_ui_select_up_03.wav",
+    "sfx_rrui_watch_ui_select_up_04.wav",
+    "sfx_rrui_watch_ui_select_up_05.wav",
+    "sfx_rrui_watch_ui_select_up_06.wav",
 ];
 
 var click, hover, release;
+
 click = new Audio();
 click.volume = 0.2;
 click.type = "audio/wav";
@@ -47,13 +48,13 @@ function sfxplay(type) {
     let x = getRndInteger(0, 6);
     switch (type) {
         case "click":
-            click.src = clicksfxs[x];
+            click.src = "https://cohenkirbyultra.github.io/testingroom/audio/ui/" + clicksfxs[x];
             click.cloneNode().play();
         case "hover":
-            hover.src = hoversfxs[x];
+            hover.src = "https://cohenkirbyultra.github.io/testingroom/audio/ui/" + hoversfxs[x];
             hover.cloneNode().play();
         case "release":
-            release.src = releasesfxs[x];
+            release.src = "https://cohenkirbyultra.github.io/testingroom/audio/ui/" + releasesfxs[x];
             release.cloneNode().play();
         default:
         // nothing happens
