de20a5b79abd37e4192631414c3a6977c6d33047
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-04-21 22:53:54 -0300

    e

--- pages/src/admin_sfx.js
+++ pages/src/admin_sfx.js
@@ -1,10 +1,11 @@
 // For login.html
 var audio = new Audio();
+audio.type = "audio/wav";
+
 function playSound(src) {
-    audio.src = src;
-    audio.type = "audio/wav";
+    audio.src = "https://cohenkirbyultra.github.io/testingroom/audio/ui/admin/" + src;
     audio.cloneNode().play();
 }
 
-document.getElementById("Username").addEventListener("change", playSound("https://cohenkirbyultra.github.io/testingroom/audio/ui/admin/Poker_DiceClack03.wav"));
-document.getElementById("Password").addEventListener("change", playSound("https://cohenkirbyultra.github.io/testingroom/audio/ui/admin/Poker_DiceClack03.wav"));
+document.getElementById("Username").addEventListener("change", playSound("Poker_DiceClack03.wav"));
+document.getElementById("Password").addEventListener("change", playSound("Poker_DiceClack03.wav"));
