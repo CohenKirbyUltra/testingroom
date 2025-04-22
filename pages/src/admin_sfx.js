19733d6422cffc82bc2164e37fc96756725e4b27
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-04-21 22:42:08 -0300

    q

--- pages/src/admin_sfx.js
+++ pages/src/admin_sfx.js
@@ -2,7 +2,7 @@
 function playSound(src) {
     let audio = new Audio()
     audio.src = src;
-    audio.cloneNode.play();
+    audio.cloneNode().play();
 }
 
 document.getElementById("Username").addEventListener("input", playSound("../audio/ui/admin/Poker_DiceClack03.wav"));
