adc6950e5650d6dd2bf581bf01a8d9e93bd781b6
CohenKS <76218057+CohenKirbyUltra@users.noreply.github.com>
2025-04-21 22:38:06 -0300

    d

--- pages/src/admin_sfx.js
+++ pages/src/admin_sfx.js
@@ -1,18 +1,9 @@
 // For login.html
-var KEYUP = false;
 function playSound(src) {
     let audio = new Audio()
     audio.src = src;
     audio.cloneNode.play();
 }
 
-document.addEventListener("keydown", function () {
-    if (KEYUP == false) {
-        playSound("../audio/ui/admin/Poker_DiceClack03.wav");
-        KEYUP = true;
-    }
-});
-
-document.addEventListener("keyup", function () {
-        KEYUP = false;
-});
+document.getElementById("Username").addEventListener("input", playSound("../audio/ui/admin/Poker_DiceClack03.wav"));
+document.getElementById("Password").addEventListener("input", playSound("../audio/ui/admin/Poker_DiceClack03.wav"));
